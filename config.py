a99fea022c79f743912574472c81a453b9f2a946
Jyprakash18 <jayPrakash092006@gmail.com>
2026-07-03 17:35:30 +0530

    Fix indentation in config.py

--- config.py
+++ config.py
@@ -58,7 +58,7 @@ class Settings:
             "3m": Plan("3m", "3 Months", 3, getenv("PLAN_3M_AMOUNT", "₹149")),
             "6m": Plan("6m", "6 Months", 6, getenv("PLAN_6M_AMOUNT", "₹269")),
             "12m": Plan("12m", "1 Year", 12, getenv("PLAN_12M_AMOUNT", "₹499")),
-            )
+        }
             return cls(
             bot_token=bot_token,
             admin_ids=admin_ids,
